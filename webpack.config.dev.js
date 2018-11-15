197e8933207424e9faa5c77c5deb8589a96ba6c4
isChosen <isChosen@163.com>
2018-11-15 22:40:47 +0800

    update 2018-11-15 22:40:43‘

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,36 +1,27 @@
-/**
- * @Author: detcx 
- * @Date: 2018-09-30 09:44:59 
- * @Last Modified by: Chosen
- * @Last Modified time: 2018-11-02 17:37:16
- * @description development configuration
- */
-
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 module.exports = {
-  mode: 'development', // development production
+  mode: "development",
   devtool: false,
-  entry: './src/entry.jsx',
+  entry: "./src/entry.jsx", // 此处使用 jsx 语法文件作为入口
   output: {
-    filename: 'js/[name].bundle.js',
-    chunkFilename: 'js/[name].[chunkhash:6].js', // 'js/[id].bundle[chunkhash:6].js'
-    path: path.resolve(__dirname, 'dist'), // 打包后的目录，必须是绝对路径
-    publicPath: '/'
-    
+    filename: "js/[name].bundle[hash:6].js",
+    chunkFilename: "js/[name][chunkhash:6].js", // 'js/[id].bundle[chunkhash:6].js'
+    path: path.resolve(__dirname, "dist"), // 打包后的目录，必须是绝对路径
+    publicPath: "/"
   },
   watchOptions: {
     ignored: /node_modules/
   },
   optimization: {
     splitChunks: {
-      chunks: 'all',
-      automaticNameDelimiter: '-'
+      chunks: "all",
+      automaticNameDelimiter: "-"
     }
   },
   module: {
@@ -38,47 +29,47 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: "babel-loader"
       }
     ]
   },
 
   devServer: {
+    hot: true,
     open: true,
-    port: '8053',
     https: false,
-    publicPath: '/',
-    contentBase: path.resolve(__dirname, 'dist'),
-    host: 'localhost',
-    disableHostCheck: true,
-    historyApiFallback: true
+    port: "8053",
+    publicPath: "/",
+    host: "localhost",
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "dist")
   },
 
   resolve: {
-    modules: [path.resolve(__dirname, 'node_modules')],
-    extensions: ['.js', '.jsx', '.es6'],
-    mainFields: ['jsnext:main', 'browser', 'main'] // 配合 scope hoisting
+    modules: [path.resolve(__dirname, "node_modules")],
+    extensions: [".js", ".jsx", ".es6"],
+    mainFields: ["jsnext:main", "browser", "main"] // 配合 scope hoisting
   },
 
   plugins: [
     new webpack.DllReferencePlugin({
-      manifest: require('./dist/dll/react.manifest.json')
+      manifest: require("./dist/dll/react.manifest.json")
     }),
     new webpack.DllReferencePlugin({
-      manifest: require('./dist/dll/polyfill.manifest.json')
+      manifest: require("./dist/dll/polyfill.manifest.json")
     }),
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      title: 'Oh-webpack-onDemand',
-	  favicon: __dirname + '/favicon.ico',
-      template: __dirname + '/index.html'
+      filename: "index.html",
+      title: "Oh-webpack-onDemand",
+      favicon: __dirname + "/favicon.ico",
+      template: __dirname + "/index.html"
     }),
-    new CleanWebpackPlugin(['dist'], {exclude: ['dll']}),
-    // scope hoisting
-    new webpack.optimize.ModuleConcatenationPlugin(),
+    new CleanWebpackPlugin(["dist"], { exclude: ["dll"] }),
+    new webpack.optimize.ModuleConcatenationPlugin() // scope hoisting
     /* new BundleAnalyzerPlugin({
       analyzerPort: 2018,
       generateStatsFile: true
     }) */
   ]
-}
+};
